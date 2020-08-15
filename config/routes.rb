74a9e549d7f0149ebb0acd780198b2ba4824982a
KBY7777777 <koba.yu.7777777@gmail.com>
2020-08-15 14:51:30 +0900

    files have been revised

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  #下記で「root〜」を設定したので、「get 'messages/index'」は削除した（コードreviewでご指摘を受けて）。
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #応用カリNo89の実演にて、indexアクションのroutingを追記
